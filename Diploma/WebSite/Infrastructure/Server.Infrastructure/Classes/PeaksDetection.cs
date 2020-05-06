03df44cd3769506bb22bb8494f21fb0050450fd7
Grykhno Arthur <60880847+arthurgrykhno@users.noreply.github.com>
2020-05-06 17:29:13 +0300

    Butterworth

---
+++ Diploma/WebSite/Infrastructure/Server.Infrastructure/Classes/PeaksDetection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Infrastructure.Classes
+{
+    public static class PeaksDetection
+    {
+        public static Task Wavelet(List<string> Points)
+        {
+            int fs = 360;
+            double f1 = 0.5, f2 = 35;
+            double[] Wn = new double[] { f1 * 2 / fs, f2 * 2 / fs };
+
+            double N = 3;
+            var ab = (0, 0);
+
+            
+
+        }
+
+        private static double[] ButterWorth(double n, double[] Wn)
+        {
+            int fs = 2;
+            double[] u = new double[] { 2 * fs * Math.Tan(Math.PI * Wn[0] / fs), 2 * fs * Math.Tan(Math.PI * Wn[1] / fs) };
+            int bType = 2;
+            double Bw = u[1] - u[0];
+            double wn = Math.Sqrt(u[0] * u[2]);
+            
+        }
+    }
+}
