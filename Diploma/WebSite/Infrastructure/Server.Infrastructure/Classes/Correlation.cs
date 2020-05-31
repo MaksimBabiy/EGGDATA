e8f0d6207d12810c5ae75c6121f9ba6f8088b5f5
Grykhno Arthur <60880847+arthurgrykhno@users.noreply.github.com>
2020-05-31 17:23:32 +0300

    Added correlation class and packages

---
+++ Diploma/WebSite/Infrastructure/Server.Infrastructure/Classes/Correlation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathNet.Numerics.Statistics;
+
+namespace Server.Infrastructure.Classes
+{
+    public static class Correlation
+    {
+        public static double Pearson(IEnumerable<double> dataA, IEnumerable<double> dataB)
+        {
+            int n = 0;
+            double r = 0.0;
+            double meanA = dataA.Mean();
+            double meanB = dataB.Mean();
+            double sdevA = dataA.StandardDeviation();
+            double sdevB = dataB.StandardDeviation();
+
+            IEnumerator<double> ieA = dataA.GetEnumerator();
+            IEnumerator<double> ieB = dataB.GetEnumerator();
+
+            while (ieA.MoveNext())
+            {
+                if (ieB.MoveNext() == false)
+                {
+                    throw new ArgumentOutOfRangeException("Datasets dataA and dataB need to have the same length.");
+                }
+
+                n++;
+                r += (ieA.Current - meanA) * (ieB.Current - meanB) / (sdevA * sdevB);
+            }
+            if (ieB.MoveNext() == true)
+            {
+                throw new ArgumentOutOfRangeException("Datasets dataA and dataB need to have the same length.");
+            }
+
+            return r / (n - 1);
+        }
+
+    }
+}
